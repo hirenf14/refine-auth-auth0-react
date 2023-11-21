1046baf714f44e470ed252bbe83d13464d52c1d3
hirenf14 <hirenf14@gmail.com>
2023-11-21 17:25:56 +0530

    feat: added auth0 provider

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
+import { Auth0Provider } from "@auth0/auth0-react";
 export {
   useAuthProvider,
   type AuthProviderHookResult,
 } from "./hooks/useAuthProvider";
+
+export const RefineAuth0Provider = Auth0Provider;
+export default RefineAuth0Provider;
